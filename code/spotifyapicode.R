db4e068f2bc93bf4155f0574f66d9d8cd6fbb58a
Gina Seo <138332474+ginaaseo@users.noreply.github.com>
2024-12-08 16:38:13 -0500

    Update spotifyapicode.R

--- code/spotifyapicode.R
+++ code/spotifyapicode.R
@@ -9,4 +9,14 @@ library(tidyr)
 library(ggplot2) 
 library(spotifyr)
 library(stats)
-library(shiny)
+library(shiny) 
+
+Sys.setenv(SPOTIFY_CLIENT_ID = "d7bf5087238d4244ab5f6091a1070ae1") 
+Sys.setenv(SPOTIFY_CLIENT_SECRET = "a1e8f0ea71274b28b204683433c28c05") 
+
+access_token <- get_spotify_access_token() 
+
+my_playlists <- get_user_playlists() 
+
+discover_weekly <- my_playlists[my_playlists$name == "Discover Weekly", ]
+tracks_discover_weekly <- get_playlist_tracks(discover_weekly$id)
